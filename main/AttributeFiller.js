54c9305b0b306cad41c27cff33f2788d1b0dc371
PawelDabrowski83 <pawel.d.dabrowski@gmail.com>
2021-08-25 22:55:08 +0200

    Refactor AttributeFiller with new helper method.

--- main/AttributeFiller.js
+++ main/AttributeFiller.js
@@ -1,12 +1,32 @@
+function generateAttributes() {
+    while(!checkAttributes()) {
+        incrementRandomAttribute();
+    }
+    console.log("Attributes set.");
+}
+
 function checkAttributes() {
-    console.log(sumAttributes());
+    return sumAttributes == 56;
 }
 
 function sumAttributes() {
     let sum = 0;
 
     for (let i = 1; i <= 6; i++) {
-        sum += sheet.getRange(ATTR_ROW, i).getValue();
+        sum += getAttribute(i);
     }
     return sum;
 }
+
+function incrementRandomAttribute() {
+    let randomAttr;
+
+    do {
+        randomAttr = randomInteger(1, 6);
+    } while(!isAttrIncrementable(getAttribute(randomAttr)));
+}
+
+function isAttrIncrementable(value) {
+    return value < 12;
+}
+
