b3fbfbd206033cbc952823499290a3e07217909b
PawelDabrowski83 <pawel.d.dabrowski@gmail.com>
2021-08-25 23:16:08 +0200

    Add Utils for incrementing Disciplines.

--- main/Utils.js
+++ main/Utils.js
@@ -9,3 +9,19 @@ function getAttribute(col) {
 function setAttribute(col, val) {
     sheet.getRange(ATTR_ROW, col).setValue(val);
 }
+
+function incrementAttribute(col) {
+    setAttribute(col, getAttribute(col) + 1);
+}
+
+function getDiscipline(col) {
+    return sheet.getRange(DISC_ROW, col).getValue();
+}
+
+function setDiscipline(col, val) {
+    sheet.getRange(DISC_ROW, col).setValue(val);
+}
+
+function incrementDiscipline(col) {
+    setDiscipline(col, getDiscipline(col) + 1);
+}
